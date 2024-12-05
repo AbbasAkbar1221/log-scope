62f5ca365791917139ca767087c307bfae493be9
AbbasAkbar1221 <abbasakbar765@gmail.com>
2024-12-05 16:15:26 +0530

    clean app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,3 @@
-// import React from 'react';
-// import LogUploadForm from './components/LogUploadForm';
-// import LogSearchForm from './components/LogSearchForm';
-
-// const App = () => {
-//   return (
-//     <div>
-//       <h1 className=''>Log File Manager</h1>
-//       <LogUploadForm />
-//       <LogSearchForm />
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
 import React from 'react';
 import LogUploadForm from './components/LogUploadForm';
 import LogSearchForm from './components/LogSearchForm';
@@ -23,7 +6,7 @@ const App = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center p-6">
       <h1 className="text-4xl font-bold text-blue-600 mb-8 text-center">
-        Log File Manager
+        Log Scope
       </h1>
       <div className="w-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="bg-white shadow-lg rounded-md p-6">
